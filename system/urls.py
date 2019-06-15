0df434ed4826b3a97d03c8b0d38cdc66b3c92717
Daniel Corcoran <danielc.92@outlook.com>
2019-06-15 20:22:02 +1000

    Added forms

--- system/urls.py
+++ system/urls.py
@@ -4,5 +4,7 @@ from .views import home_view, login_view, register_view
 app_name = 'system'
 
 urlpatterns = [
-    
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
+    path('', home_view, name='home'),
 ]
