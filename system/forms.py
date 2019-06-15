b479cb71960546ad26895c69ef8817af4ddc18d0
Daniel Corcoran <danielc.92@outlook.com>
2019-06-15 20:16:49 +1000

    Added content for login view

--- system/forms.py
+++ system/forms.py
@@ -1,4 +1,4 @@
-from djdango.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
--- system/views.py
+++ system/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from .forms import RegisterForm
+from .forms import RegisterForm as RF
 from django.http import HttpResponse
+from django.contrib.auth import login as authlogin
+from django.contrib.auth import logout as authlogout
+from django.contrib.auth import authenticate as auth
+from django.contrib.auth.decorators import login_required as lr
 
-# Create your views here.
 
 
+@lr
 def home_view(request):
 
     context = {}
@@ -13,6 +17,18 @@ def home_view(request):
 
 
 def login_view(request):
+
+    if request.method == 'POST':
+
+        user = auth(request, 
+        username=request.POST.get('uname'),
+        password=request.POST.get('pass'))
+
+        if user is not None:
+            authlogin(request, user)
+            return redirect('home')
+        else:
+            return HttpResponse('You have failed to login.')
 
     context = {}
     
@@ -23,13 +39,13 @@ def login_view(request):
 def register_view(request):
 
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
+        form = RF(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponse('Success')
             
     else:
-        form = RegisterForm()
+        form = RF()
 
     context = {'form': form}
     
