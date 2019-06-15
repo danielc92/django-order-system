16392bc62262798042dda6bcff96225f82e5d5f6
Daniel Corcoran <danielc.92@outlook.com>
2019-06-15 20:08:10 +1000

    Added views

---
+++ system/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import home_view, login_view, register_view
+
+app_name = 'system'
+
+urlpatterns = [
+    
+]
--- system/views.py
+++ system/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view(request):
+
+    context = {}
+
+    return render(request, 'home.html', context)
+
+
+def login_view(request):
+
+    context = {}
+    
+    return render(request, 'login.html', context)
+
+
+
+def register_view(request):
+
+    context = {}
+    
+    return render(request, 'register.html', context)
+
